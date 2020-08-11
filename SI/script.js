27447d0450043f4b1c884d197fdd89ca9f69c65b
pokhrelrajat <37318838+pokhrelrajat@users.noreply.github.com>
2020-08-11 11:56:12 +0545

    modified

--- SI/script.js
+++ SI/script.js
@@ -1,18 +1,18 @@
 //functions
-function timeFunction(){
-	var x = document.getElementById('month').value;
-	if (x=month) {
-	let t= t/12;
-	}
-
+function timeFunction() {
+  var x = document.getElementById("month").value;
+  if ((x = month)) {
+    let t = t / 12;
+  }
 }
 
 function interest() {
-	let p = document.getElementById('p').value;
-	let t = document.getElementById('t').value;
-	let r = document.getElementById('r').value;
+  let p = document.getElementById("p").value;
+  let t = document.getElementById("t").value;
+  let r = document.getElementById("r").value;
 
-	let i = (p*t*r)/100;
+  let i = (p * t * r) / 100;
 
-	document.getElementById('info').innerHTML = i;
+  document.getElementById("info").innerHTML = i;
+  alert("The simple interest is: " + i);
 }
